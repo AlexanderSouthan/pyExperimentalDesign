68098532403ff6f9cc70c705920f7fdd43e026d8
Southan <alexander.southan@igb.fraunhofer.de>
2021-10-30 02:31:13 +0200

    Adapted for this repository

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='little_helpers',
+    name='pyExperimentalDesign',
     version='0.0.1',
     packages=find_packages(where='src'),
-    install_requires=['numpy', 'scikit-learn', 'scipy', 'statsmodels']
+    install_requires=['numpy', 'pandas', 'scipy', 'statsmodels']
 )
