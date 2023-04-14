0d897bf86262b1786280729266b359752f3e605a
Alexander Southan <72788772+AlexanderSouthan@users.noreply.github.com>
2023-04-14 14:25:43 +0200

    Added pyDataFitting requirement

--- setup.py
+++ setup.py
@@ -4,5 +4,5 @@ setup(
     name='pyExperimentalDesign',
     version='0.0.1',
     packages=find_packages(where='src'),
-    install_requires=['numpy', 'pandas', 'scipy', 'statsmodels']
+    install_requires=['numpy', 'pandas', 'scipy', 'statsmodels', 'pyDataFitting']
 )
